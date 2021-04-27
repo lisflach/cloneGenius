eb8c5df6833f6facff14f552af4a435fa610c86c
Lisandra Flach da Rosa <lisflach@gmail.com>
2021-04-27 03:43:26 -0300

    commit inicial

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Clone Genius 🕹️
+
+------
+
+- Jogo da memória estilo Genius;
+- Estudo de HTML, CSS,  JavaScript;
+- Atividade do Digital Innovation One.
---
+++ script.js
@@ -0,0 +1,95 @@
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+const azul = document.querySelector('.azul');
+const vermelho = document.querySelector('.vermelho');
+const verde = document.querySelector('.verde');
+const amarelo = document.querySelector('.amarelo');
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4);
+    order[order.length] = colorOrder;
+    clickedOrder = [];
+
+    for(let i in order) {
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
+    }
+}
+
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number - 250);
+    setTimeout(() => {
+        element.classList.remove('selected');
+    });
+}
+
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder[i] != order[i]) {
+            gameOver();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length) {
+        alert(`Pontuação: ${score}\nVocê acertou!!! Iniciando próximo nível...`);
+        nextLevel();
+    }
+}
+
+let click = (color) => {
+    clickedOrder[clickedOrder.length] = color;
+    createColorElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createColorElement(color).classList.remove('selected');
+        checkOrder();
+    } ,250);
+}
+
+let createColorElement = (color) => {
+    if(color == 0) {
+        return verde;
+    } else if(color == 1) {
+        return vermelho;
+    } else if (color == 2) {
+        return amarelo;
+    } else if (color == 3) {
+        return azul;
+    }
+}
+
+let nextLevel = () => {
+    score++;
+    shuffleOrder();
+}
+
+let gameOver = () => {
+    alert(`Pontuação: ${score}!\nVocê perdeu o jogo!\nClique em OK para iniciar um novo jogo!`);
+    order = [];
+    clickedOrder = [];
+    playGame();
+}
+
+let playGame = () => {
+    alert('Bem vindo ao Genius!!! Iniciando novo jogo...');
+    score = 0;
+    nextLevel();
+}
+
+verde.onclick = () => click(0);
+vermelho.onclick = () => click(1);
+amarelo.onclick = () => click(2);
+azul.onclick = () => click(3);
+
+playGame();
